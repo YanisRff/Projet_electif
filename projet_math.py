f9ffa8a91bebcda69ca24e596c7c5092be803638
PierreZboril-Berteaud <pierre.zboril-berteaud@isen-ouest.yncrea.fr>
2025-06-02 14:51:45 +0200

    architecture

--- projet_math.py
+++ projet_math.py
@@ -1,49 +1,5 @@
-from math import sqrt
-from math import inf
-import matplotlib.pyplot as plt
+from fonctions import *
 
-def mean(arr):
-    #Calcule la moyenne en prenant un tableau de int en entrée
-    sum=0
-    for i in range(len(arr)):
-        sum+= arr[i]
-    return sum/len(arr)
-
-def mediane(arr):
-    n = len(arr)
-    if n%2==0:
-        mediane = (arr[n//2] + arr[(n//2) + 1])/2
-    else:
-        mediane = arr[(n+1)//2]
-    return mediane
-def variance(arr,mean):
-    # Calcule la variance en prenant un tableau de int en entrée ainsi que la moyenne de ce même tableau
-    var = 0
-    for i in range(len(arr)):
-        var+= (arr[i]-mean)*(arr[i]-mean)
-    return var/len(arr)
-
-def ecart_type(variance):
-    # Calcule la ecart-type en prenant une variance en entrée
-    return sqrt(variance)
-
-def min(arr):
-    min = inf
-    for i in range(len(arr)):
-        if arr[i] < min:
-            min = arr[i]
-
-    return min
-
-def max(arr):
-    max = -inf
-    for i in range(len(arr)):
-        if arr[i] > max:
-            max = arr[i]
-    return max
-
-def etendue(max,min):
-    return max-min
 
 if __name__ == '__main__':
 
