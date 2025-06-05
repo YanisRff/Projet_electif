89dcbbf501001f9cfe6703c5cd0e223aafffe5b4
PierreZboril-Berteaud <pierre.zboril-berteaud@isen-ouest.yncrea.fr>
2025-06-05 08:31:10 +0200

    a

--- algo_CAH.py
+++ algo_CAH.py
@@ -38,12 +38,6 @@ if __name__ == '__main__':
     print("labels =", labels)
     print("points =", points)
 
-    #(X_min, Y_min), d_min = dist_min(points, dist_euclidienne)
-    #print(f"Paire la plus proche : {X_min} – {Y_min}  (distance = {d_min:.2f})")
-
-
-    #(X_m1, Y_m1), d_m1 = dist_min(points, dist_euclidienne)
-    #print(f"Paire la plus proche (distance Manhattan) : {X_m1} – {Y_m1}  (d = {d_m1:.2f})")
 
     (X_m1, Y_m1), d_m1 = dist_min(points, dist_euclidienne)
 
@@ -111,3 +105,4 @@ if __name__ == '__main__':
             for label, cluster in results_dbscan:
                 print(f"{label} → Cluster {cluster}")
 
+    repaire(points)
